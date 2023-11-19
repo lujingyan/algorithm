2247fb9d95b0e88344f757b8cbb8bbaaf1025c37
lujingyan <lujingyanann@gmail.com>
2023-11-19 12:13:30 +0800

    commit 11/19

--- src/main/java/com/example/Solution643.java
+++ src/main/java/com/example/Solution643.java
@@ -1,5 +1,15 @@
 package com.example;
 
 public class Solution643 {
-
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for(int i = 0; i < k; i ++)
+            sum += nums[i];
+        int maxSum = sum;
+        for(int i = k; i < nums.length; i ++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double)maxSum / k;
+    }
 }
